d0514394a60a3e498edad6131aed61c7e80720aa
Antonov333 <antonovsergei333@yahoo.com>
2023-07-12 15:18:08 +0300

    Initial Commit

---
+++ src/main/java/com/example/course2lesson16algorithmspart1/Course2Lesson16AlgorithmsPart1Application.java
@@ -0,0 +1,13 @@
+package com.example.course2lesson16algorithmspart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2Lesson16AlgorithmsPart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2Lesson16AlgorithmsPart1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/course2lesson16algorithmspart1/Course2Lesson16AlgorithmsPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course2lesson16algorithmspart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2Lesson16AlgorithmsPart1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
